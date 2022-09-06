e0a273ab1c8f454c9c4a87904ff23a39ca4e3ee0
Lavaviki <lavanya.ramesh.java@gmail.com>
2022-09-06 13:07:42 +0530

    lava code

--- src/test/java/org/test/Sample1.java
+++ src/test/java/org/test/Sample1.java
@@ -6,10 +6,11 @@ public class Sample1 {
 		
 			System.out.println("Test1");
 			System.out.println("Test2");
-			System.out.println("Test3");
-			System.out.println("Test4");
 			System.out.println("Test5");
-			System.out.println("Test6");		
+			System.out.println("Test6");
+			System.out.println("I Love India");
+			System.out.println("Project Class");
+			System.out.println("Testing a new Project");
 	}
 
 }
