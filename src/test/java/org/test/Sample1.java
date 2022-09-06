8d4c67d4a1f40ee90de367bdaf8c6aebdb365a27
Lavaviki <lavanya.ramesh.java@gmail.com>
2022-09-06 12:42:41 +0530

    Project Review

--- src/test/java/org/test/Sample1.java
+++ src/test/java/org/test/Sample1.java
@@ -6,7 +6,10 @@ public class Sample1 {
 		
 			System.out.println("Test1");
 			System.out.println("Test2");
-			System.out.println("Test3"); 
+			System.out.println("Test3");
+			System.out.println("Test4");
+			System.out.println("Test5");
+			System.out.println("Test6");		
 	}
 
 }
